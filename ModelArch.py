a99d32eb425c6d6aaf84c0f3b1f9538e5560e786
Mafciejewicz <melonik0.9846@gmail.com>
2021-05-06 02:28:46 +0200

    Add different model architectures

---
+++ ModelArch.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ModelArch(Enum):
+    ARCH_1 = 1
+    ARCH_2 = 2
+    ARCH_3 = 3
--- model.py
+++ model.py
@@ -1,7 +1,23 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from typing import Tuple, Dict
+from ModelArch import ModelArch
 
-def make_model(input_shape):
+def get_checkpoint_dir(arch: ModelArch) -> str:
+    return {
+        ModelArch.ARCH_1: "arch1",
+        ModelArch.ARCH_2: "arch2",
+        ModelArch.ARCH_3: "arch3"
+    }[arch]
+
+def make_model(input_shape: Tuple[int, int, int], arch: ModelArch) -> Sequential:
+    return {
+        ModelArch.ARCH_1: make_model_arch1(input_shape),
+        ModelArch.ARCH_2: make_model_arch2(input_shape),
+        ModelArch.ARCH_3: make_model_arch3(input_shape)
+    }[arch]
+
+def make_model_arch1(input_shape: Tuple[int, int, int]):
     model=Sequential()
 
     model.add(Conv2D(filters=4, kernel_size=(3,3), activation="relu", input_shape=input_shape))
@@ -14,6 +30,43 @@ def make_model(input_shape):
     model.add(Conv2D(filters=32, kernel_size=(3,3), activation="relu"))
     model.add(Flatten())
     model.add(Dense(units=256, activation="relu"))
+    model.add(Dense(units=36, activation="softmax"))
+
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    return model
+
+def make_model_arch2(input_shape: Tuple[int, int, int]) -> Sequential:
+    model=Sequential()
+
+    model.add(Conv2D(filters=4, kernel_size=(3,3), activation="relu", input_shape=input_shape))
+    model.add(Conv2D(filters=4, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=8, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=8, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=16, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=16, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=32, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=32, kernel_size=(3,3), activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(units=256, activation="relu"))
+    model.add(Dense(units=36, activation="softmax"))
+
+    model.compile(optimizer="SGD", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    return model
+
+def make_model_arch3(input_shape: Tuple[int, int, int]) -> Sequential:
+    model=Sequential()
+
+    model.add(Conv2D(filters=4, kernel_size=(3,3), activation="relu", input_shape=input_shape))
+    model.add(Conv2D(filters=4, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=8, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=8, kernel_size=(3,3), activation="relu"))
+    model.add(Conv2D(filters=16, kernel_size=(3,3), activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(units=256, activation="relu"))
+    model.add(Dense(units=128, activation="relu"))
+    model.add(Dense(units=128, activation="relu"))
     model.add(Dense(units=36, activation="softmax"))
 
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
